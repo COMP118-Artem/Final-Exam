ca04cbe12dc66bfff48f1672392e0e582adeb2d8
Artem Samusev <samusev.a@live.unic.ac.cy>
2021-05-28 10:42:13 +0300

    add enter array and chacged function

--- Final Exam.cpp
+++ Final Exam.cpp
@@ -6,11 +6,17 @@ int main()
 {
 	bool notOver = true;
 	int comm = 0;
-	int arg1 = 0;
-	int arg2 = 0;
+	int num1 = 0;
+	int num2 = 0;
 	int summ = 0;
 	int arr[15];
- 
+
+	cout << "Enter array: ";
+
+	for (int i = 0; i < 15; i++)
+	{
+		cin >> arr[i];
+	}
 
 
 
